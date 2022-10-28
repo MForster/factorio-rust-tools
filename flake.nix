25190c3cc378227aa0d23a0e86fa9185676bb8d0
Michael Forster <email@michael-forster.de>
2022-10-28 15:28:12 +0200

    Set up development environment

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default =
+          mkShell { buildInputs = [ cargo cargo-watch rustfmt clippy ]; };
+      });
+}
